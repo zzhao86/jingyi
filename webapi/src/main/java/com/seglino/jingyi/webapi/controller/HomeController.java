fdd8adf346c598b692c5c8da59f7251f374cf82c
zzhao86 <zzhao86@163.com>
2019-06-20 17:42:26 +0800

    2019年6月20日17:42:21

--- webapi/src/main/java/com/seglino/jingyi/webapi/controller/HomeController.java
+++ webapi/src/main/java/com/seglino/jingyi/webapi/controller/HomeController.java
@@ -1,19 +1,27 @@
 package com.seglino.jingyi.webapi.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.seglino.jingyi.common.response.ApiResult;
+import com.seglino.jingyi.dingtalk.utils.DingtalkBean;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class HomeController {
-	@ResponseBody
-	@RequestMapping("/")
+
+	@GetMapping("/")
 	public ApiResult index() {
 		ApiResult aResult = new ApiResult();
 		aResult.setData("this is webapi home page");
+		return aResult;
+	}
+
+	@GetMapping("/dt/token")
+	public ApiResult dingtalkToken() {
+		ApiResult aResult = new ApiResult();
+		aResult.setData(DingtalkBean.AccessToken);
 		return aResult;
 	}
 }
