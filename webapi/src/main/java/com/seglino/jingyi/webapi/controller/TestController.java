0b2a14395cfef0f2f56a001951226e95b8114a49
zzhao86 <zzhao86@163.com>
2019-08-22 17:37:39 +0800

    2019年8月22日17:36:38

--- webapi/src/main/java/com/seglino/jingyi/webapi/controller/TestController.java
+++ webapi/src/main/java/com/seglino/jingyi/webapi/controller/TestController.java
@@ -1,11 +1,27 @@
 package com.seglino.jingyi.webapi.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.seglino.jingyi.common.response.ApiResult;
+import com.seglino.jingyi.common.utils.ExcelUtils;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
-
+	
+	@PostMapping("import")
+	public ApiResult importExcel(HttpServletRequest request) {
+		ApiResult aResult = new ApiResult();
+		try {
+			ExcelUtils.
+		} catch (Exception e) {
+			aResult.addError(e);
+		}
+		return aResult;
+	}
 
 }
