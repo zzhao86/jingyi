9db8c6d6ee7d8c36bb670dabf9dc0decf9cc178d
zzhao86 <zzhao86@163.com>
2019-09-06 09:50:17 +0800

    2019年9月6日09:50:15

--- common/src/main/java/com/seglino/jingyi/common/excel/ExportExcel.java
+++ common/src/main/java/com/seglino/jingyi/common/excel/ExportExcel.java
@@ -1,5 +1,9 @@
 package com.seglino.jingyi.common.excel;
 
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFFont;
@@ -11,29 +15,62 @@ import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.util.CellRangeAddress;
 
 public class ExportExcel<T> {
-	private String sheetName;
+	/**
+	 * 工作簿名称
+	 */
+	private String sheetName = "sheet1";
+	/**
+	 * 数据表格标题
+	 */
 	private String title;
-	private String[] headers;
 
-	public byte[] exportExcel() {
+	public ExportExcel() {
+	}
+
+	public ExportExcel(String title) {
+		this.title = title;
+	}
+
+	public ExportExcel(String sheetName, String title) {
+		this.sheetName = sheetName;
+		this.title = title;
+	}
+
+	/**
+	 * 导出数据到Excel
+	 * @param dataset 导出数据集合
+	 * @param headMap 表格列名和数据字段名称对应Map。key：数据类字段名称，value：表格列名
+	 * @return
+	 */
+	public byte[] exportExcel(List<T> dataset, Map<String, String> headMap) {
 		HSSFWorkbook workbook = new HSSFWorkbook();
 		// 创建工作簿
 		HSSFSheet sheet = workbook.createSheet(sheetName);
 		// 设置默认列宽为20
 		sheet.setDefaultColumnWidth(20);
 
-		// 创建标题
+		// 创建数据表格标题
 		HSSFRow titleRow = sheet.createRow(0);
 		titleRow.setHeightInPoints(30);
 		HSSFCell titleCell = titleRow.createCell(0);
 		titleCell.setCellValue(title);
 		titleCell.setCellStyle(getTitleCellStyle(workbook));
-		sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, headers.length));
+		sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, headMap.size()));
 
-		return null;
+		// 创建表格列名
+		
+		// 
+		for (T t : dataset) {
+			Field[] fields = t.getClass().getDeclaredFields();
+			for (Field field : fields) {
+				
+			}
+		}
+
+		return workbook.getBytes();
 	}
 
-	// 标题样式
+	// 数据表格标题样式
 	private HSSFCellStyle getTitleCellStyle(HSSFWorkbook workbook) {
 		HSSFCellStyle style = workbook.createCellStyle();
 		style.setAlignment(HorizontalAlignment.CENTER);
@@ -44,4 +81,5 @@ public class ExportExcel<T> {
 		style.setFont(font);
 		return style;
 	}
+
 }
--- webapi/src/main/java/com/seglino/jingyi/webapi/controller/TestController.java
+++ webapi/src/main/java/com/seglino/jingyi/webapi/controller/TestController.java
@@ -1,10 +1,75 @@
 package com.seglino.jingyi.webapi.controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.seglino.jingyi.assets.service.AssetsService;
+import com.seglino.jingyi.common.excel.ExportExcel;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
+	@Autowired
+	private AssetsService assetsService;
 
+	@GetMapping("export")
+	public void export(HttpServletResponse response) {
+		assetsService.list(new HashMap<String, Object>());
+		ExportExcel<AssestExport> ee = new ExportExcel<AssestExport>();
+//		ee.exportExcel();
+	}
+
+	public class AssestExport {
+		private Object id;
+		private Long code;
+		private String name;
+		private String brand;
+		private String model;
+
+		public Object getId() {
+			return id;
+		}
+
+		public void setId(Object id) {
+			this.id = id;
+		}
+
+		public Long getCode() {
+			return code;
+		}
+
+		public void setCode(Long code) {
+			this.code = code;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getBrand() {
+			return brand;
+		}
+
+		public void setBrand(String brand) {
+			this.brand = brand;
+		}
+
+		public String getModel() {
+			return model;
+		}
+
+		public void setModel(String model) {
+			this.model = model;
+		}
+	}
 }
