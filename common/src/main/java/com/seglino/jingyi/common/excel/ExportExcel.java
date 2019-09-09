6695ba2c347683c876a0444a2da6d8d15ded61ef
zzhao86 <zzhao86@163.com>
2019-09-09 17:34:50 +0800

    2019年9月9日17:34:47

--- common/src/main/java/com/seglino/jingyi/common/excel/ExportExcel.java
+++ common/src/main/java/com/seglino/jingyi/common/excel/ExportExcel.java
@@ -1,18 +1,23 @@
 package com.seglino.jingyi.common.excel;
 
-import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
-import org.apache.poi.hssf.usermodel.HSSFFont;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExportExcel<T> {
 	/**
@@ -22,7 +27,12 @@ public class ExportExcel<T> {
 	/**
 	 * 数据表格标题
 	 */
-	private String title;
+	private String title = "导出数据";
+
+	/**
+	 * 表格是否需要添加边框
+	 */
+	private boolean isBorder = true;
 
 	public ExportExcel() {
 	}
@@ -38,48 +48,209 @@ public class ExportExcel<T> {
 
 	/**
 	 * 导出数据到Excel
+	 * 
 	 * @param dataset 导出数据集合
 	 * @param headMap 表格列名和数据字段名称对应Map。key：数据类字段名称，value：表格列名
 	 * @return
 	 */
-	public byte[] exportExcel(List<T> dataset, Map<String, String> headMap) {
-		HSSFWorkbook workbook = new HSSFWorkbook();
+	public XSSFWorkbook exportExcel(List<T> dataset, Map<String, String> headMap) {
+		XSSFWorkbook workbook = new XSSFWorkbook();
 		// 创建工作簿
-		HSSFSheet sheet = workbook.createSheet(sheetName);
+		XSSFSheet sheet = workbook.createSheet(sheetName);
 		// 设置默认列宽为20
 		sheet.setDefaultColumnWidth(20);
 
 		// 创建数据表格标题
-		HSSFRow titleRow = sheet.createRow(0);
-		titleRow.setHeightInPoints(30);
-		HSSFCell titleCell = titleRow.createCell(0);
+		XSSFRow titleRow = sheet.createRow(0);
+		titleRow.setHeightInPoints(50);
+		XSSFCell titleCell = titleRow.createCell(0);
 		titleCell.setCellValue(title);
 		titleCell.setCellStyle(getTitleCellStyle(workbook));
-		sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, headMap.size()));
+		sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, headMap.size() - 1));
 
 		// 创建表格列名
-		
-		// 
-		for (T t : dataset) {
-			Field[] fields = t.getClass().getDeclaredFields();
-			for (Field field : fields) {
-				
-			}
+		XSSFRow headRow = sheet.createRow(1);
+		headRow.setHeightInPoints(30);
+		int index = 0;
+		for (Entry<String, String> entry : headMap.entrySet()) {
+			XSSFCell headCell = headRow.createCell(index++);
+			headCell.setCellValue(entry.getValue());
+			headCell.setCellStyle(getHeadCellStyle(workbook));
 		}
 
-		return workbook.getBytes();
+		// 创建导出数据
+		int dataRowIndex = 2;
+		for (T t : dataset) {
+			int dataCellIndex = 0;
+			XSSFRow dataRow = sheet.createRow(dataRowIndex++);
+			dataRow.setHeightInPoints(20);
+			for (Entry<String, String> entry : headMap.entrySet()) {
+				Method[] methods = t.getClass().getMethods();
+				XSSFCell dataCell = dataRow.createCell(dataCellIndex++);
+				for (Method method : methods) {
+					String getMethodName = "get" + entry.getKey().substring(0, 1).toUpperCase() + entry.getKey().substring(1);
+					if (getMethodName.equals(method.getName())) {
+						try {
+							String typeName = method.getReturnType().getName();
+							Object value = method.invoke(t);
+							XSSFCellStyle style = getDataCellStyle(workbook);
+							if (null != value) {
+								switch (typeName) {
+								case "java.lang.Integer":
+									dataCell.setCellValue((Integer) value);
+									break;
+								case "java.lang.Long":
+									dataCell.setCellValue((Long) value);
+									break;
+								case "java.lang.Short":
+									dataCell.setCellValue((Short) value);
+									break;
+								case "java.lang.Double":
+									dataCell.setCellValue((Double) value);
+									break;
+								case "java.lang.Float":
+									dataCell.setCellValue((Float) value);
+									break;
+								case "java.util.Date":
+									dataCell.setCellValue((Date) value);
+									style.setDataFormat(workbook.createDataFormat().getFormat("yyyy/mm/dd"));
+									break;
+								default:
+									dataCell.setCellValue(value + "");
+									break;
+								}
+							} else {
+								dataCell.setBlank();
+							}
+							dataCell.setCellStyle(style);
+							break;
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+		return workbook;
 	}
 
-	// 数据表格标题样式
-	private HSSFCellStyle getTitleCellStyle(HSSFWorkbook workbook) {
-		HSSFCellStyle style = workbook.createCellStyle();
+	/**
+	 * 表格标题样式
+	 * 
+	 * @param workbook
+	 * @return
+	 */
+	private XSSFCellStyle getTitleCellStyle(XSSFWorkbook workbook) {
+		XSSFCellStyle style = getBaseCellStyle(workbook);
 		style.setAlignment(HorizontalAlignment.CENTER);
 		style.setVerticalAlignment(VerticalAlignment.CENTER);
-		HSSFFont font = workbook.createFont();
+		XSSFFont font = workbook.createFont();
 		font.setBold(true);
 		font.setFontHeightInPoints((short) 16);
 		style.setFont(font);
 		return style;
 	}
 
+	/**
+	 * 表格列头单元格样式
+	 * 
+	 * @param workbook
+	 * @return
+	 */
+	private XSSFCellStyle getHeadCellStyle(XSSFWorkbook workbook) {
+		XSSFCellStyle style = getBaseCellStyle(workbook);
+		style.setFillForegroundColor(IndexedColors.PALE_BLUE.getIndex());
+		style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		style.setAlignment(HorizontalAlignment.CENTER);
+		style.setVerticalAlignment(VerticalAlignment.CENTER);
+		XSSFFont font = workbook.createFont();
+		font.setBold(true);
+		font.setFontHeightInPoints((short) 11);
+		style.setFont(font);
+		return style;
+	}
+
+	/**
+	 * 表格表格数据单元格样式
+	 * 
+	 * @param workbook
+	 * @return
+	 */
+	private XSSFCellStyle getDataCellStyle(XSSFWorkbook workbook) {
+		XSSFCellStyle style = getBaseCellStyle(workbook);
+
+		return style;
+	}
+
+	/**
+	 * 基础样式
+	 * 
+	 * @param workbook
+	 * @return
+	 */
+	private XSSFCellStyle getBaseCellStyle(XSSFWorkbook workbook) {
+		XSSFCellStyle style = workbook.createCellStyle();
+		if (isBorder) {
+			style.setBorderTop(BorderStyle.THIN);
+			style.setBorderRight(BorderStyle.THIN);
+			style.setBorderBottom(BorderStyle.THIN);
+			style.setBorderLeft(BorderStyle.THIN);
+		}
+		style.setVerticalAlignment(VerticalAlignment.CENTER);
+		return style;
+	}
+
+	/**
+	 * 获取工作簿名称
+	 * 
+	 * @return
+	 */
+	public String getSheetName() {
+		return sheetName;
+	}
+
+	/**
+	 * 设置工作簿名称
+	 * 
+	 * @param sheetName
+	 */
+	public void setSheetName(String sheetName) {
+		this.sheetName = sheetName;
+	}
+
+	/**
+	 * 获取数据表格标题
+	 * 
+	 * @return
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * 设置数据表格标题
+	 * 
+	 * @param title
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * 获取是否添加边框
+	 * 
+	 * @return
+	 */
+	public boolean getIsBorder() {
+		return isBorder;
+	}
+
+	/**
+	 * 设置是否添加边框
+	 * 
+	 * @param isBorder
+	 */
+	public void setIsBorder(boolean isBorder) {
+		this.isBorder = isBorder;
+	}
 }
--- webapi/src/main/java/com/seglino/jingyi/webapi/controller/TestController.java
+++ webapi/src/main/java/com/seglino/jingyi/webapi/controller/TestController.java
@@ -1,9 +1,17 @@
 package com.seglino.jingyi.webapi.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.seglino.jingyi.assets.service.AssetsService;
 import com.seglino.jingyi.common.excel.ExportExcel;
+import com.seglino.jingyi.common.utils.DateUtils;
 
 @RestController
 @RequestMapping("test")
@@ -20,17 +29,47 @@ public class TestController {
 
 	@GetMapping("export")
 	public void export(HttpServletResponse response) {
-		assetsService.list(new HashMap<String, Object>());
-		ExportExcel<AssestExport> ee = new ExportExcel<AssestExport>();
-//		ee.exportExcel();
+		List<AssetsExport> list = new ArrayList<AssetsExport>();
+		list.add(new AssetsExport(1, "测试1", "品牌1", "型号1", "2019-9-9 11:50:07"));
+		list.add(new AssetsExport(2, "测试2", "品牌2", "型号2", "2019-9-9 11:50:07"));
+		list.add(new AssetsExport(3, "测试3", "品牌3", "型号3", "2019-9-9 11:50:07"));
+		list.add(new AssetsExport(4, "测试4", "品牌4", "", "2019-9-9 11:50:07"));
+		Map<String, String> headMap = new HashMap<String, String>();
+		headMap.put("code", "编码");
+		headMap.put("name", "名称");
+		headMap.put("brand", "品牌");
+		headMap.put("model", "型号");
+		headMap.put("date", "日期");
+		ExportExcel<AssetsExport> ee = new ExportExcel<AssetsExport>("导出测试");
+		XSSFWorkbook workbook = ee.exportExcel(list, headMap);
+		try {
+			String ext = "." + workbook.getWorkbookType().name();
+			response.setHeader("Content-Disposition", "attachment;Filename=" + System.currentTimeMillis() + ext);
+			workbook.write(response.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public class AssestExport {
+	public class AssetsExport {
+		public AssetsExport() {
+
+		}
+
+		public AssetsExport(long code, String name, String brand, String model, String date) {
+			this.code = code;
+			this.name = name;
+			this.brand = brand;
+			this.model = model;
+			this.date = DateUtils.toDate(date);
+		}
+
 		private Object id;
 		private Long code;
 		private String name;
 		private String brand;
 		private String model;
+		private Date date;
 
 		public Object getId() {
 			return id;
@@ -70,6 +109,14 @@ public class TestController {
 
 		public void setModel(String model) {
 			this.model = model;
+		}
+
+		public Date getDate() {
+			return date;
+		}
+
+		public void setDate(Date date) {
+			this.date = date;
 		}
 	}
 }
